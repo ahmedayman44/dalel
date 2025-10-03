c5028ce5b5e8ae9028b81568c40d4613950e326e
ahmedayman44 <ahmedayman203011@gmail.com>
2025-10-03 23:39:05 +0300

    refactor our code

--- lib/features/auth/presentation/screens/sign_up_view.dart
+++ lib/features/auth/presentation/screens/sign_up_view.dart
@@ -1,8 +1,7 @@
-import 'package:dalel/core/utils/app_color.dart';
 import 'package:dalel/core/utils/app_strings.dart';
-import 'package:dalel/core/utils/app_text_style.dart';
 import 'package:dalel/core/widgets/custom_button.dart';
 import 'package:dalel/features/auth/presentation/widgets/custom_text_field.dart';
+import 'package:dalel/features/auth/presentation/widgets/have_an_account_widget.dart';
 import 'package:dalel/features/auth/presentation/widgets/terms_and_condition.dart';
 import 'package:dalel/features/auth/presentation/widgets/welcome_text_widget.dart';
 import 'package:flutter/material.dart';
@@ -46,35 +45,7 @@ class SignUpView extends StatelessWidget {
             SliverToBoxAdapter(
               child: HaveAnAccountWidget(
                 text1: AppStrings.alreadyHaveAnAccount,
-                text2: AppStrings.signUp,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HaveAnAccountWidget extends StatelessWidget {
-  const HaveAnAccountWidget({
-    super.key,
-    required this.text1,
-    required this.text2,
-  });
-  final String text1;
-  final String text2;
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      child: Text.rich(
-        TextSpan(
-          children: [
-            TextSpan(text: text1, style: Styles.poppins400style12),
-            TextSpan(
-              text: text2,
-              style: Styles.poppins400style12.copyWith(
-                color: AppColor.lightGrey,
+                text2: AppStrings.signIn,
               ),
             ),
           ],
---
+++ lib/features/auth/presentation/widgets/have_an_account_widget.dart
@@ -0,0 +1,31 @@
+import 'package:dalel/core/utils/app_color.dart';
+import 'package:dalel/core/utils/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+class HaveAnAccountWidget extends StatelessWidget {
+  const HaveAnAccountWidget({
+    super.key,
+    required this.text1,
+    required this.text2,
+  });
+  final String text1;
+  final String text2;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(text: text1, style: Styles.poppins400style12),
+            TextSpan(
+              text: text2,
+              style: Styles.poppins400style12.copyWith(
+                color: AppColor.lightGrey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
